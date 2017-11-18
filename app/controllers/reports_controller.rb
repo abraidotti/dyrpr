6c0f357d047aabb8a46ebb2ec7479e436c3d7a1c
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-11-17 23:43:53 -0500

    add update and destroy methods

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,20 @@
 class ReportsController < ApplicationController
+  before_action :current_report, only: [:show, :update, :delete]
+
+  def create
+    @report = Report.new(report_params)
+    @report.pilot_id = current_pilot.id
+
+    if @report.save
+      flash[:notice] = 'Yay! Saved!'
+      redirect_to reports_path
+    else
+      flash[:notice] = "Error. Report not saved: #{@report.errors.messages.values}"
+      redirect_to new_report_path
+    end
+  end
+
   def show
-    report_id = params[:id]
-    @report = Report.find(report_id)
   end
 
   def index
@@ -12,28 +25,22 @@ class ReportsController < ApplicationController
     @report = Report.new
   end
 
-  def create
-    @report = Report.new(report_params)
-    @report.pilot_id = current_pilot.id
+  def destroy
+    current_report.destroy
+    redirect_to reports_path
+  end
 
-    if @report.save
-      flash[:notice] = 'Yay! Saved!'
-      redirect_to new_report_path
-    else
-      flash[:notice] = "Error. Report not saved: #{@report.errors.messages.values}"
-      redirect_to new_report_path
-    end
-
-    def update
-
-
-    end
-
+  def update
+    current_report.update(report_params)
+    redirect_to report_path(current_report.id)
   end
 
   private
 
-
+  def current_report
+    report_id = params[:id]
+    @report = Report.find(report_id)
+  end
 
   def report_params
     params.require(:report).permit(:title, :message)
